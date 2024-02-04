ef62038bc7a2478ba95bd5aa8a903257d54e2730
Jeffin03 <jeffyissac2203@gmail.com>
2024-02-05 00:47:16 +0530

    added topbar to dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { TopbarComponent } from "./topbar/topbar.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { ContentViewComponent } from "./content-view/content-view.component";
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+    selector: 'app-dashboard',
+    standalone: true,
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.css',
+    imports: [TopbarComponent, SidebarComponent, ContentViewComponent]
 })
 export class DashboardComponent {
 
