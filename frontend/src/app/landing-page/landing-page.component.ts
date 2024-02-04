9c2abc501f3f03b43c3f6005552ad8d09c80fd2d
Jeffin03 <jeffyissac2203@gmail.com>
2024-02-04 20:54:41 +0530

    update

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,22 +1,12 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './navbar/navbar.component';
-import { IntroSectionComponent } from './intro-section/intro-section.component';
 
 @Component({
     selector: 'app-landing-page',
     standalone: true,
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.css',
-    imports: [NavbarComponent]
+    imports: []
 })
 export class LandingPageComponent {
-  // Declare instances of the sub components
-  navbarComponent: NavbarComponent;
-  introSectionComponent: IntroSectionComponent;
-
-  constructor() {
-    // Initialize the sub components
-    this.navbarComponent = new NavbarComponent();
-    this.introSectionComponent = new IntroSectionComponent();
+  
   }
-}
